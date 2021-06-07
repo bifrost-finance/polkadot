63a8c1f921e02b5a4f2042586f58a0ec18cc6e60
Allen Pocket <AllenPocketWork@Gmail.com>
2021-06-07 12:52:10 +0800

    :sparkles: ($RUNTIME) Impl Sudo & ParasSudoWrapper for kusama runtime (#2)
    Closes

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -32,7 +32,7 @@ use primitives::v1::{
 	InboundDownwardMessage, InboundHrmpMessage, SessionInfo,
 };
 use runtime_common::{
-	claims, paras_registrar, xcm_sender, slots, auctions, crowdloan,
+	claims, paras_sudo_wrapper, paras_registrar, xcm_sender, slots, auctions, crowdloan,
 	SlowAdjustingFeeUpdate, CurrencyToVote, impls::DealWithFees,
 	BlockHashCount, RocksDbWeight, BlockWeights, BlockLength, OffchainSolutionWeightLimit, OffchainSolutionLengthLimit,
 	ToAuthor,
@@ -914,6 +914,11 @@ impl pallet_vesting::Config for Runtime {
 	type WeightInfo = weights::pallet_vesting::WeightInfo<Runtime>;
 }
 
+impl pallet_sudo::Config for Runtime {
+	type Event = Event;
+	type Call = Call;
+}
+
 parameter_types! {
 	// One storage item; key size 32, value size 8; .
 	pub const ProxyDepositBase: Balance = deposit(1, 8);
@@ -932,6 +937,7 @@ pub enum ProxyType {
 	NonTransfer,
 	Governance,
 	Staking,
+	SudoBalances,
 	IdentityJudgement,
 	CancelProxy,
 }
@@ -1006,6 +1012,11 @@ impl InstanceFilter<Call> for ProxyType {
 				Call::Session(..) |
 				Call::Utility(..)
 			),
+			ProxyType::SudoBalances => match c {
+				Call::Sudo(pallet_sudo::Call::sudo(ref x)) => matches!(x.as_ref(), &Call::Balances(..)),
+				Call::Utility(..) => true,
+				_ => false,
+			},
 			ProxyType::IdentityJudgement => matches!(c,
 				Call::Identity(pallet_identity::Call::provide_judgement(..)) |
 				Call::Utility(..)
@@ -1084,6 +1095,8 @@ impl parachains_initializer::Config for Runtime {
 	type Randomness = pallet_babe::RandomnessFromOneEpochAgo<Runtime>;
 	type ForceOrigin = EnsureRoot<AccountId>;
 }
+
+impl paras_sudo_wrapper::Config for Runtime {}
 
 parameter_types! {
 	pub const ParaDeposit: Balance = deposit(10, MAX_CODE_SIZE + MAX_HEAD_SIZE);
@@ -1404,6 +1417,9 @@ construct_runtime! {
 		// Claims. Usable initially.
 		Claims: claims::{Pallet, Call, Storage, Event<T>, Config<T>, ValidateUnsigned} = 19,
 
+		// Sudo.
+		Sudo: pallet_sudo::{Pallet, Call, Storage, Event<T>, Config<T>} = 21,
+
 		// Utility module.
 		Utility: pallet_utility::{Pallet, Call, Event} = 24,
 
@@ -1459,6 +1475,7 @@ construct_runtime! {
 		Slots: slots::{Pallet, Call, Storage, Event<T>} = 71,
 		Auctions: auctions::{Pallet, Call, Storage, Event<T>} = 72,
 		Crowdloan: crowdloan::{Pallet, Call, Storage, Event<T>} = 73,
+		ParasSudoWrapper: paras_sudo_wrapper::{Pallet, Call} = 74,
 
 		// Pallet for sending XCM.
 		XcmPallet: pallet_xcm::{Pallet, Call, Storage, Event<T>} = 99,
